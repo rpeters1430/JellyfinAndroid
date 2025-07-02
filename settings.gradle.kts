517021ead1e95ec421c9bbe006fabc185f7195c0
Ryan Peters <rpeters1428@gmail.com>
2025-07-01 18:19:56 -0700

    feat: Update Kotlin and Hilt versions, add serialization and KSP plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,14 @@
 pluginManagement {
+    plugins {
+        id("org.jetbrains.kotlin.android") version "2.0.0"
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
+        id("com.google.devtools.ksp") version "2.0.0-1.0.20"
+        id("com.google.dagger.hilt.android") version "2.51"
+        id("com.android.application") version "8.5.2"
+    }
+
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
